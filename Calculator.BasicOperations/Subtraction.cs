5d46f6bfb2f60840fb3f79e40c1eb22f7d601839
Vinicius de Souza <viniciusdesouzafernando@gmail.com>
2023-12-13 15:47:39 -0300

    Update Subtraction.cs

--- Calculator.BasicOperations/Subtraction.cs
+++ Calculator.BasicOperations/Subtraction.cs
@@ -8,5 +8,10 @@ namespace Calculator.BasicOperations
 {
     internal class Subtraction
     {
+        public static void Run(decimal number1, decimal number2)
+        {
+            decimal result = number1 - number2;
+            Console.WriteLine($"O resultado da subtração de {number1} por {number2} é: {result}");
+        }
     }
 }
