d3b19859df117045661d935ffac74c95bbe5faa9
armentanoc <armentanocarolina@gmail.com>
2023-12-13 15:41:29 -0300

    Add multiply and sum classes.

---
+++ Calculator.BasicOperations/Multiply.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Calculator.BasicOperations
+{
+    internal class Multiply
+    {
+        public static void Run(decimal number1, decimal number2) 
+        {
+            decimal result = number1 * number2;
+            Console.WriteLine($"O resultado da multiplicação de {number1} por {number2} é: {result}");
+        }
+    }
+}
---
+++ Calculator.BasicOperations/Sum.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Calculator.BasicOperations
+{
+    internal class Sum
+    {
+        public static void Run(decimal number1, decimal number2)
+        {
+            decimal result = number1 + number2;
+            Console.WriteLine($"O resultado da soma de {number1} por {number2} é: {result}");
+        }
+    }
+}
