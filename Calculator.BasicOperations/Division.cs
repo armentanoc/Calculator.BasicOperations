a5ca599945263aa91b5989e1434653176daff55d
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2023-12-13 15:54:10 -0300

    atualizando o código

--- Calculator.BasicOperations/Division.cs
+++ Calculator.BasicOperations/Division.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Calculator.BasicOperations
 {
-    internal class Division
+    public class Division
     {
+        public static void Run(decimal number1, decimal number2)
+        {
+            if(number1 < number2 || number2 == 0)
+            {
+                Console.WriteLine("Não é possível realizar essa divisão");
+            }
+            else
+            {
+                Console.WriteLine($"A divisão de {number1} por {number2} é igual a:{number1/number2}");
+            }
+        }
     }
 }
